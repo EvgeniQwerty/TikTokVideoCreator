82b93f49f98155fc75be4ba32034212cd0e5359b
EvgeniQwerty <evgeniqwerty1337@gmail.com>
2022-03-06 15:23:35 +0300

    Add command line arguments and some fixes

--- generate.py
+++ generate.py
@@ -3,12 +3,13 @@ from pytube import YouTube
 from moviepy.editor import *
 from os import getcwd, listdir
 from random import randint
+import argparse
+import sys
 
 
 def downloadVideosFromPlaylist(playlist_url):
     print('Downloading videos from playlist')
     playlist = Playlist(playlist_url)
-    playlist = playlist[:10]
     print('{} videos in the playlist'.format(len(playlist)))
     for video in playlist:
 
@@ -28,7 +29,7 @@ def downloadVideosFromPlaylist(playlist_url):
 
 #generate text for the video
 def concatTxt(artist_name, track_title, splitter=''):
-    text = ""
+    text = ''
 
     if track_title.find('[') >= 0:
         track_title = track_title.split('[')[0]
@@ -38,29 +39,29 @@ def concatTxt(artist_name, track_title, splitter=''):
         if track_title.find('(') >= 0:
             track_title = track_title.split('(')
             if len(track_title) == 2:
-                text = "Techno Track\nof the Day\n\n{} &\n{} -\n{}\n({}".format(artist_name[0].strip(), artist_name[1].strip(),
+                text = 'Techno Track\nof the Day\n\n{} &\n{} -\n{}\n({}'.format(artist_name[0].strip(), artist_name[1].strip(),
                                                                                track_title[0].strip(), track_title[1].strip())
             else:
-                text = "Techno Track\nof the Day\n\n{} &\n{} -\n{}".format(artist_name[0].strip(), artist_name[1].strip(),
+                text = 'Techno Track\nof the Day\n\n{} &\n{} -\n{}'.format(artist_name[0].strip(), artist_name[1].strip(),
                                                                           track_title[0].strip())
         else:
-            text = "Techno Track\nof the Day\n\n{} &\n{} -\n{}".format(artist_name[0].strip(), artist_name[1].strip(),
+            text = 'Techno Track\nof the Day\n\n{} &\n{} -\n{}'.format(artist_name[0].strip(), artist_name[1].strip(),
                                                                       track_title.strip())
     else:
         if track_title.find('(') >= 0:
             track_title = track_title.split('(')
             if len(track_title) == 2:
-                text = "Techno Track\nof the Day\n\n{} -\n{}\n({}".format(artist_name.strip(), track_title[0].strip(),
+                text = 'Techno Track\nof the Day\n\n{} -\n{}\n({}'.format(artist_name.strip(), track_title[0].strip(),
                                                                          track_title[1].strip())
             else:
-                text = "Techno Track\nof the Day\n\n{} -\n{}".format(artist_name.strip(), track_title[0].strip())
+                text = 'Techno Track\nof the Day\n\n{} -\n{}'.format(artist_name.strip(), track_title[0].strip())
         else:
-            text = "Techno Track\nof the Day\n\n{} -\n{}".format(artist_name.strip(), track_title.strip())
+            text = 'Techno Track\nof the Day\n\n{} -\n{}'.format(artist_name.strip(), track_title.strip())
 
     return text
 
 
-def main(playlist_url):
+def downloadMusicFromPlaylist(playlist_url):
     print('Music downloader')
     playlist = Playlist(playlist_url)
     counter = 1
@@ -71,7 +72,7 @@ def main(playlist_url):
         try:
             yt = YouTube(video)
             title = yt.title
-            print("{} track. {}, {} seconds".format(counter, yt.title, yt.length))
+            print('{} track. {}, {} seconds'.format(counter, yt.title, yt.length))
             counter += 1
 
             #скачиваем трек
@@ -93,41 +94,64 @@ def main(playlist_url):
             clip = clip.resize((1080, 1080))
             moviesize = 1080, 1080
 
-            title = yt.title.split('-')
-            txt = ""
+            title = yt.title.replace('–', '-')
+            title = title.replace('(Original Mix)', '')
+            title = title.replace('(Official Audio)', '')
+            title = title.split('-')
+            txt = ''
 
             if len(title) == 2:
                 if title[0].find(',') >= 0:
                     txt = concatTxt(title[0], title[1], ',')
                 elif title[0].find('&') >= 0:
                     txt = concatTxt(title[0], title[1], '&')
+                elif title[0].find('feat.') >= 0:
+                    txt = concatTxt(title[0], title[1], 'feat.')
                 elif title[0].find('feat') >= 0:
                     txt = concatTxt(title[0], title[1], 'feat')
+                elif title[0].find(' x ') >= 0:
+                    txt = concatTxt(title[0], title[1], ' x ')
                 else:
                     txt = concatTxt(title[0], title[1])
             else:
                 # if title do not consist of name of artist or name of the track
-                print("ERROR. Title do not consists of name of artist or name of the track")
+                print('ERROR. Title do not consists of name of artist or name of the track')
                 continue
 
-            clip_txt = TextClip(txt, color='white', align='West', fontsize=80, font='Candara-Bold-Italic', method='label',
+            clip_txt = TextClip(txt, color='white', align='West', fontsize=80, font='Arial-Bold', method='label',
                                 stroke_color='black', stroke_width=3)
 
             final = CompositeVideoClip([clip, clip_txt.set_pos(('center', 'center'))], size=moviesize)
             final = final.set_audio(music)
-            final.set_duration(7).write_videofile("final//{}.mp4".format(yt.title))
+            final.set_duration(7).write_videofile('final//{}.mp4'.format(yt.title))
 
         except:
-            print("ERROR while downloading or creating clip")
+            print('ERROR while downloading or creating clip')
             pass
 
 
-if __name__ == "__main__":
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dv', '--downloadVisuals', default='false')
+    parser.add_argument('-vpu', '--visualsPlaylistURL',
+                        default='https://www.youtube.com/playlist?list=PLCqJefJxoW8wRGX7iAgImBcz084j9wM0h')
+    parser.add_argument('-dm', '--downloadMusic', default='true')
+    parser.add_argument('-mpu', '--musicPlaylistURL',
+                        default='https://www.youtube.com/playlist?list=PLUGshIgZ8go9HT0dIUqnw3T2TjVB69ba8')
+
+    return parser
+
+if __name__ == '__main__':
     print('-------------------------')
     print('TikTok Video Creator v0.2')
     print('-------------------------')
     print()
-    #visuals_playlist_url = "https://www.youtube.com/playlist?list=PLCqJefJxoW8wRGX7iAgImBcz084j9wM0h"
-    #downloadVideosFromPlaylist(visuals_playlist_url)
-    music_playlist_url = 'https://www.youtube.com/playlist?list=PLUGshIgZ8go9HT0dIUqnw3T2TjVB69ba8'
-    main(music_playlist_url)
+
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    if namespace.downloadVisuals == 'true':
+        downloadVideosFromPlaylist(namespace.visualsPlaylistURL)
+
+    if namespace.downloadMusic == 'true':
+        downloadMusicFromPlaylist(namespace.musicPlaylistURL)
